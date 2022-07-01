d76dd5123a2f6b7e30581746531be1757ceab577
Ray Ortiz <oray@bgsu.edu>
2022-06-30 21:12:48 -0400

    contract name updated and comments lesson

---
+++ contracts/MyContract.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.0;
+
+// Normal single line comment
+/// NatSpec single line comment
+
+/*
+Normal multi-line comment
+*/
+
+/**
+NatSpec multi-line comment
+*/
+
+
+/// @title The best smart contract.
+/// @author The HMI Guy.
+/// @dev Please use Hashlips's video series.
+contract MyContract {
+    
+}
