d4249b8948a625730ea05e7c6979feb1d1018402
Ray Ortiz <oray@bgsu.edu>
2022-07-04 18:44:41 -0400

    comments added

--- contracts/MyVariables.sol
+++ contracts/MyVariables.sol
@@ -4,6 +4,9 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
+/// @title Contract was built following Hashlip's video series.
+/// @author The HMI Guy.
+/// @dev Please use Hashlips's video series.
 contract MyVariable {
-    
+
 }
