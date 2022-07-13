4614f054873e2115fc84394e6756521ad6df6ef8
Ray Ortiz <oray@bgsu.edu>
2022-07-13 07:51:21 -0400

    Working from the video part 3

--- contracts/MyVariables.sol
+++ contracts/MyVariables.sol
@@ -8,5 +8,18 @@ pragma solidity ^0.8.0;
 /// @author The HMI Guy.
 /// @dev Please use Hashlips's video series.
 contract MyVariable {
+    uint256 number = 20; //state variable (stored on the blockchain forever)
+    bool paused = true; //state variable
+    uint256 time = block.timestamp; //state variable saving a global variabel
 
+    function myFunct() public view{
+        //uint256 localNumber = 20; //local variables (memory)
+        //bool localPaused = true; //local variables
+        //address theSenderAddress = msg.sender;
+    }
+}
+
+contract INeedVariables is MyVariable{
+    uint256 public theAwesomeVar = 100;
+    uint256 public theTime = time;
 }
