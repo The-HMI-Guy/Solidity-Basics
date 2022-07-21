1561d50133f2a80cec35aa3693aecdcedbd5996c
Ray Ortiz <oray@bgsu.edu>
2022-07-21 07:13:26 -0400

    Part 4 lessons

--- contracts/MyOperators.sol
+++ contracts/MyOperators.sol
@@ -8,4 +8,32 @@ contract MyOperators {
     uint8 a = 1;
     uint8 b = 2;
     uint8 c = 3;
+    uint8 newValue;
+
+    /// @notice Arithemtic Operators (+, -, *, /, %, ++, --)
+    function example_1() public view{
+        a * b + c;
+        c % b;
+    }
+
+    /// @notice Assignment Operators (=, +=, -+, *=, /+, %=)
+    function example_2() public {
+        newValue = a * b + c;
+        a = a * b + c;
+        b = b * c;
+        b *= c; // same as the above statement
+    }
+
+    /// @notice Comparison Operators (==, !=, <, <=, >, >=)
+    function example_3() public {
+
+    }
+    /// @notice Logical Operators (!, &&, !!)
+    function example_4() public {
+
+    }
+    /// @notice Conditional Operators (? A:B)
+    function example_5() public {
+
+    }
 }
