c0f6ae8062743d0484f6cbf44dd20373c3bde70d
Ray Ortiz <oray@bgsu.edu>
2022-07-26 21:27:22 -0400

    Part 6 - Finished For Loops

--- contracts/MyLoop.sol
+++ contracts/MyLoop.sol
@@ -3,4 +3,22 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
-contract MyLoop {}
+contract MyLoop {
+    function myLoop() public pure {
+        //pure not reference any local or global variables
+        //identifier, condition, iterate
+        for (uint256 i = 0; i < 5; i++) {
+            if (i == 3) {
+                break;
+            }
+            if (i == 2) {
+                continue; //skips to the next iteration in the loop i.e. 2 => 3
+            }
+        }
+        uint256 j = 0;
+        while (j < 10) {
+            //Do any actions
+            j++;
+        }
+    }
+}
