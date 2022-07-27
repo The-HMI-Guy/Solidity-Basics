02794c67f9a87f70aca3e42aadc4e828302cf613
Ray Ortiz <oray@bgsu.edu>
2022-07-27 07:07:56 -0400

    Part 6 - My Array

---
+++ contracts/MyArray.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.0;
+
+contract MyArray {
+    uint256[] public arr_1;
+    uint256[] public arr_2 = [3,2,5];
+    uint256[5] public arr_3;
+}
