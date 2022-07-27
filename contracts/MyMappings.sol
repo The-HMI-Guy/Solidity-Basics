436824f53d2256ec1960662bbef5acda723e99c2
Ray Ortiz <oray@bgsu.edu>
2022-07-26 21:41:12 -0400

    Part 6 - Mappings

--- contracts/MyMappings.sol
+++ contracts/MyMappings.sol
@@ -3,4 +3,16 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
-contract MyMappings {}
+contract MyMappings {
+    mapping(uint256 => address) private nfts; //mapping signature takes key value pair. Set to private to avoid default getter. Function below used for owner.
+    uint256 counter = 0;
+
+    function getOwnerOfNFT(uint256 _id) public view returns (address){
+        return nfts[_id];
+    }
+
+    function mint(address _address)public {
+        nfts[counter] = _address;
+        counter++;
+    }
+}
