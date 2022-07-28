8df8f9c8c1388ed37744eeab40f537c6418534e5
Ray Ortiz <oray@bgsu.edu>
2022-07-28 07:44:18 -0400

    Part 7 - showing old way

--- contracts/MyStructs.sol
+++ contracts/MyStructs.sol
@@ -3,4 +3,21 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
-contract MyStructs{}
+contract MyStructs {
+    struct NFT {
+        string name;
+        uint256 dna;
+    }
+    NFT[] public nftList;
+
+    function addNFT(string memory _name, uint256 _dna) public {
+        NFT memory newNFT;
+        newNFT.name = _name;
+        newNFT.dna = _dna;
+        nftList.push();
+    }
+
+    function getNFTName(uint256 _index) public view returns (NFT memory) {
+        return nftList[_index];
+    }
+}
