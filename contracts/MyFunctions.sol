3cd25e13512d0507d642a5b34b8ecea97f1233dc
Ray Ortiz <oray@bgsu.edu>
2022-08-01 07:54:41 -0400

    PART 7 - MyFunctions contract

---
+++ contracts/MyFunctions.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.0;
+
+contract MyFunctions {
+    uint256 myUint = 42;
+    string myString = "Daniel";
+    bool myBool = true;
+    uint256[] myArr = [3,3,3];
+
+}
