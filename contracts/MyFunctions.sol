6636b93f501d8d3696c36701fe98d05822f06934
Ray Ortiz <oray@bgsu.edu>
2022-08-02 07:34:01 -0400

    PART 7 - Different Functions

--- contracts/MyFunctions.sol
+++ contracts/MyFunctions.sol
@@ -7,10 +7,22 @@ contract MyFunctions {
     uint256 myUint = 42;
     string myString = "Daniel";
     bool myBool = true;
-    uint256[] myArr = [3,3,3];
+    uint256[] myArr = [3, 3, 3];
 
-    function myFunc () public {
-        
+    function myPureFunc(uint256 _x, uint256 _y)
+        public
+        pure
+        returns (uint256 result)
+    {
+        return _x + _y;
     }
 
+    function myViewFunc() public view returns (string memory) {
+        return myString;
+    }
+
+    function myUpdateFunc() public returns (string memory) {
+        myString = "Hellow Wrold";
+        return myString;
+    }
 }
