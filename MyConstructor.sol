7b89cfba5b3e91f3c55bca806375eba1181e9e9b
Ray Ortiz <oray@bgsu.edu>
2022-08-03 07:54:41 -0400

    PART 9 - Constructor and inheritance

--- MyConstructor.sol
+++ MyConstructor.sol
@@ -4,5 +4,13 @@ import "hardhat/console.sol";
 pragma solidity ^0.8.0;
 
 contract MyConstructor {
-    
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
+
+contract MySecondContract is MyConstructor {
+    constructor(string memory _name) MyConstructor(_name) {}
 }
