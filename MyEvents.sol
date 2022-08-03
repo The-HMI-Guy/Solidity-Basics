808e75dee3efcb3ad55888451251a4f1339c506e
Ray Ortiz <oray@bgsu.edu>
2022-08-03 07:43:01 -0400

    PART 9 - Events overview

--- MyEvents.sol
+++ MyEvents.sol
@@ -4,5 +4,10 @@ import "hardhat/console.sol";
 pragma solidity ^0.8.0;
 
 contract MyEvents {
-    
+    event CreatedNFT(address indexed user, uint256 id, uint256 dna);
+
+    function createNFT(uint256 _id, uint256 _dna) public {
+        //create the nft and save to the storage
+        emit CreatedNFT(msg.sender, _id, _dna);
+    }
 }
