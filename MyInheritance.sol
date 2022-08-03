76fb8d71f95da3e8af06785a74bd44047f881135
Ray Ortiz <oray@bgsu.edu>
2022-08-03 07:55:59 -0400

    PART 9 - Inheritance overview

---
+++ MyInheritance.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.0;
+
+contract MyInheritance_A {}
+
+contract MyInheritance_B {}
+
+contract MyInheritance_C {}
