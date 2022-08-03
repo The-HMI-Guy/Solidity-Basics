c4a0d1c1238cb6d96e5a0c5e374c528e101023f5
Ray Ortiz <oray@bgsu.edu>
2022-08-03 07:34:57 -0400

    PART 9 - Modifiers overview

--- contracts/MyModifier.sol
+++ contracts/MyModifier.sol
@@ -6,8 +6,29 @@ pragma solidity ^0.8.0;
 contract MyModifier {
     uint256 public myNum = 0;
     bool public paused = false;
+    address public owner;
 
-    function addToNum() public {
+    constructor() {
+        owner = address(msg.sender);
+    }
+
+    modifier isNotPaused(bool _bypass) {
+        if (_bypass == true) {
+            require(paused == false, "Contract is paused");
+            _;
+        }
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    function setPaused() public {
+        paused = true;
+    }
+
+    function addToNum() public isNotPaused(false) {
+        //not a good practice of passing in the hard value. Best to haev variable.
         myNum++;
     }
 }
