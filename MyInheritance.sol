a8c6e9e7c74ebddfdadefd129b299d89a18451b1
Ray Ortiz <oray@bgsu.edu>
2022-08-03 18:18:12 -0400

    PART 9 - Override

--- MyInheritance.sol
+++ MyInheritance.sol
@@ -3,8 +3,20 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
-contract MyInheritance_A {}
+contract MyInheritance_A {
+    string internal name = "Hashlips";
+}
 
-contract MyInheritance_B {}
+contract MyInheritance_B is MyInheritance_A {
+    function getName() public view virtual returns (string memory) {
+        //virtual indicates this can be overwritten
+        return name;
+    }
+}
 
-contract MyInheritance_C {}
+contract MyInheritance_C is MyInheritance_B {
+    function getName() public view virtual override returns (string memory) {
+        //virtual indicates this can be overwritten
+        return "Override!";
+    }
+}
