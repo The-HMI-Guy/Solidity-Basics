3a50a43eef3505c26e066a6f8f0cf94feeffdd3c
Ray Ortiz <oray@bgsu.edu>
2022-08-04 07:08:25 -0400

    PART 10 - Payable Overvoew

--- MyPayable.sol
+++ MyPayable.sol
@@ -6,10 +6,14 @@ pragma solidity ^0.8.0;
 contract MyPayable {
     address payable public myAddress;
 
-    constructor(){
+    constructor() {
         myAddress = payable(msg.sender);
     }
-    function deposit() public {
-        //payable(msg.sender).transfer(100);
+
+    function deposit() public payable {}
+
+    function getThisContractBalance() public view returns (uint256){
+        uint256 amount = address(this).balance;
+        return amount;
     }
 }
