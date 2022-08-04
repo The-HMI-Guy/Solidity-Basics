3ff225d82e15dca5d13db6c7626d988146df7cee
Ray Ortiz <oray@bgsu.edu>
2022-08-04 07:21:37 -0400

    PART 10 - Completed

--- MyPayable.sol
+++ MyPayable.sol
@@ -12,8 +12,27 @@ contract MyPayable {
 
     function deposit() public payable {}
 
-    function getThisContractBalance() public view returns (uint256){
+    function getThisContractBalance() public view returns (uint256) {
         uint256 amount = address(this).balance;
         return amount;
     }
+
+    //Three different options for sending Eth. Call is the preferred method.
+    function transferEth(address payable _user) public payable {
+        _user.transfer(msg.value);
+    }
+
+    function sendEth(address payable _user) public payable {
+        bool didSend = _user.send(msg.value);
+        require(didSend, "This failed to send");
+    }
+
+    function callEth(address payable _user) public payable {
+        (bool didSend, bytes memory data) = _user.call{value: msg.value}("");
+        require(didSend, "This transaction failed");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
