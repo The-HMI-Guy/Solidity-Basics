3a97c9806ee87e934b7b90cf3732715d34564a2c
Ray Ortiz <oray@bgsu.edu>
2022-08-04 06:58:36 -0400

    PART 10 - Interface

--- MyInterface.sol
+++ MyInterface.sol
@@ -3,4 +3,20 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
-contract MyInterface {}
+interface ICounter {
+    function count() external view returns (uint256);
+
+    function addToCount() external;
+}
+
+contract MyInterface is ICounter {
+    uint256 counter = 0;
+
+    function count() external view returns (uint256) {
+        return counter;
+    }
+
+    function addToCount() external {
+        counter++;
+    }
+}
