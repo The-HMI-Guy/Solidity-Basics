fdd57760914c617858d69e3b36e5972c34e65eba
Ray Ortiz <oray@bgsu.edu>
2022-08-05 07:27:22 -0400

    Part 11 - Contract Calls Oveerview

--- MyContractCalls.sol
+++ MyContractCalls.sol
@@ -3,6 +3,18 @@ import "hardhat/console.sol";
 
 pragma solidity ^0.8.0;
 
+contract MyContract {
+    uint256 public age = 29;
+
+    function getAge() public view returns (uint256) {
+        return age;
+    }
+}
+
 contract MyContractCalls {
-    
+    function getAgeFromOtherContract(address _contractAdress) public view returns(uint256) {
+        MyContract other = MyContract(_contractAdress);
+        uint256 age = other.getAge();
+        return age;
+    }
 }
